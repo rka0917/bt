2b17356d3d82142e859e5f2251f20ecbaa3cdacc
Niklas Reje <niklas.reje@gmail.com>
2018-02-26 23:30:16 +0100

    Added CC to getConnectionAsync in ConnectionSource.

--- bt-core/src/main/java/bt/net/ConnectionSource.java
+++ bt-core/src/main/java/bt/net/ConnectionSource.java
@@ -20,6 +20,8 @@ import bt.CountingThreadFactory;
 import bt.metainfo.TorrentId;
 import bt.runtime.Config;
 import bt.service.IRuntimeLifecycleBinder;
+import core.CodeCoverage;
+
 import com.google.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -83,61 +85,93 @@ public class ConnectionSource implements IConnectionSource {
 
     @Override
     public CompletableFuture<ConnectionResult> getConnectionAsync(Peer peer, TorrentId torrentId) {
+    	CodeCoverage cc = new CodeCoverage("ConnectionSource_getConnectionAsync.txt");
+    	System.out.println("#CC# getConnectionAsync 1");
         ConnectionKey key = new ConnectionKey(peer, torrentId);
 
         CompletableFuture<ConnectionResult> connection = getExistingOrPendingConnection(key);
         if (connection != null) {
+        	System.out.println("#CC# getConnectionAsync 2");
             if (connection.isDone() && LOGGER.isDebugEnabled()) {
                 LOGGER.debug("Returning existing connection for peer: {}. Torrent: {}", peer, torrentId);
+                System.out.println("#CC# getConnectionAsync 3");
             }
+            else System.out.println("#CC# getConnectionAsync 4");
+            cc.writeToFile("getConnectionAsync");
             return connection;
         }
+        else System.out.println("#CC# getConnectionAsync 5");
 
         Long bannedAt = unreachablePeers.get(peer);
         if (bannedAt != null) {
+        	System.out.println("#CC# getConnectionAsync 6");
             if (System.currentTimeMillis() - bannedAt >= config.getUnreachablePeerBanDuration().toMillis()) {
+            	System.out.println("#CC# getConnectionAsync 7");
                 if (LOGGER.isDebugEnabled()) {
+                	System.out.println("#CC# getConnectionAsync 8");
                     LOGGER.debug("Removing temporary ban for unreachable peer: {}", peer);
                 }
+                else System.out.println("#CC# getConnectionAsync 9");
                 unreachablePeers.remove(peer);
             } else {
+            	System.out.println("#CC# getConnectionAsync 10");
                 if (LOGGER.isDebugEnabled()) {
+                	System.out.println("#CC# getConnectionAsync 11");
                     LOGGER.debug("Will not attempt to establish connection to peer: {}. " +
                             "Reason: peer is unreachable. Torrent: {}", peer, torrentId);
                 }
+                else System.out.println("#CC# getConnectionAsync 12");
+                cc.writeToFile("getConnectionAsync");
                 return CompletableFuture.completedFuture(ConnectionResult.failure("Peer is unreachable"));
             }
         }
+        else System.out.println("#CC# getConnectionAsync 13");
 
         if (connectionPool.size() >= config.getMaxPeerConnections()) {
+        	System.out.println("#CC# getConnectionAsync 14");
             if (LOGGER.isDebugEnabled()) {
+            	System.out.println("#CC# getConnectionAsync 15");
                 LOGGER.debug("Will not attempt to establish connection to peer: {}. " +
                         "Reason: connections limit exceeded. Torrent: {}", peer, torrentId);
             }
+            else System.out.println("#CC# getConnectionAsync 16");
+            cc.writeToFile("getConnectionAsync");
             return CompletableFuture.completedFuture(ConnectionResult.failure("Connections limit exceeded"));
         }
+        else System.out.println("#CC# getConnectionAsync 17");
 
         synchronized (pendingConnections) {
             connection = getExistingOrPendingConnection(key);
             if (connection != null) {
+            	System.out.println("#CC# getConnectionAsync 18");
                 if (connection.isDone() && LOGGER.isDebugEnabled()) {
+                	System.out.println("#CC# getConnectionAsync 19");
                     LOGGER.debug("Returning existing connection for peer: {}. Torrent: {}", peer, torrentId);
                 }
+                else System.out.println("#CC# getConnectionAsync 20");
+                cc.writeToFile("getConnectionAsync");
                 return connection;
             }
+            else System.out.println("#CC# getConnectionAsync 21");
 
             connection = CompletableFuture.supplyAsync(() -> {
                 try {
                     ConnectionResult connectionResult =
                             connectionFactory.createOutgoingConnection(peer, torrentId);
                     if (connectionResult.isSuccess()) {
+                    	System.out.println("#CC# getConnectionAsync 22");
                         PeerConnection established = connectionResult.getConnection();
                         PeerConnection added = connectionPool.addConnectionIfAbsent(established);
                         if (added != established) {
+                        	System.out.println("#CC# getConnectionAsync 23");
                             established.closeQuietly();
                         }
+                        else System.out.println("#CC# getConnectionAsync 24");
+                        cc.writeToFile("getConnectionAsync");
                         return ConnectionResult.success(added);
                     } else {
+                    	System.out.println("#CC# getConnectionAsync 25");
+                    	cc.writeToFile("getConnectionAsync");
                         return connectionResult;
                     }
                 } finally {
@@ -147,18 +181,26 @@ public class ConnectionSource implements IConnectionSource {
                 }
             }, connectionExecutor).whenComplete((acquiredConnection, throwable) -> {
                 if (acquiredConnection == null || throwable != null) {
+                	System.out.println("#CC# getConnectionAsync 26");
                     if (LOGGER.isDebugEnabled()) {
+                    	System.out.println("#CC# getConnectionAsync 27");
                         LOGGER.debug("Peer is unreachable: {}. Will prevent further attempts to establish connection.", peer);
                     }
+                    else System.out.println("#CC# getConnectionAsync 28");
                     unreachablePeers.putIfAbsent(peer, System.currentTimeMillis());
                 }
+                else System.out.println("#CC# getConnectionAsync 29");
                 if (throwable != null) {
+                	System.out.println("#CC# getConnectionAsync 30");
                     if (LOGGER.isDebugEnabled()) {
+                    	System.out.println("#CC# getConnectionAsync 31");
                         LOGGER.debug("Failed to establish outgoing connection to peer: " + peer, throwable);
                     }
+                    else System.out.println("#CC# getConnectionAsync 32");
                 }
+                else System.out.println("#CC# getConnectionAsync 33");
             });
-
+            cc.writeToFile("getConnectionAsync");
             pendingConnections.put(key, connection);
             return connection;
         }
